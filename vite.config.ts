6bf6b68ce533338e633f907de999bc6d3aba0f63
SeungGeun02 <ksg55412222@gmail.com>
2025-12-16 16:47:09 +0900

    init: project

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,7 @@
-export default defineConfig(({ mode }) => {
-  return {
-    base: '/Chuncheon-Spring-Again/',
-    server: {
-      port: 3000,
-      host: '0.0.0.0',
-    },
-    plugins: [react()],
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, '.'),
-      }
-    }
-  };
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  base: "/-Chuncheon-Spring-Again-/",
 });
